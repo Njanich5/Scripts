3fd3cefbcc3dd7c04a87abfcd95b005a24b9f08b
Nicholas Janich <njanich5@gmail.com>
2017-01-25 11:56:02 -0600

    Add files via upload

---
+++ userlog.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+$see = "/var/log/auth.log.1";
+print "Successful User Attempt List \n
+------------- LOGIN NAMES -------------- \n
+Count		Name\n\n";
+
+open(FILEREAD, "< $see");
+while(<FILEREAD> ) {
+	chomp($_);
+	@data = split();
+	if( $data[5] eq "Invalid" ) {
+	#	printf "$data[7] \n";
+		$name = ($data[7]);
+	#	$ip = ($data[11]);		
+		
+		$count{$name}++;
+		}
+
+}
+close FILEREAD;
+
+foreach $name (sort {$count{$b} <=> $count{$a}} (keys %count)) {
+	printf "%-5s		%-5s	%-5s\n", $count{$name}, $name;
+}
+
